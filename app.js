1b72acbca16e395c352b31697286d33821c046bc
irtizaaah <irtizaaah@gmail.com>
2022-03-06 10:30:21 +0400

    Fixed typo

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ function searchPlayer(){
             labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
             datasets: [{
               label: 'Point History',
-              data: [...player.getPointHistory()],
+              data: [...player.getPointsHistory()],
               fill: true,
               backgroundColor: '#9C0F48',
               borderColor: 'rgb(255, 99, 132)',
